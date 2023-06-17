126d86275a7308cb26a72f8fc084f9ea80196e29
yussufrqbi <yussufrakkabi@gmail.com>
2023-06-17 02:59:04 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main(void)
+/**
+ * main - main code
+ * return - 0 (success)
+ */
+int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }
